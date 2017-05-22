24ccecfbcd800bcdcc3c443307dad46ca417f08e
Sky Xu <skyrocks88xmz@gmail.com>
2017-05-22 14:14:18 -0400

    add sessions

---
+++ ror-backend/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  devise_for :users, only: []
+  namespace :v1, defaults: { format: :json } do
+    resource :login, only: [:create], controller: :sessions
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
